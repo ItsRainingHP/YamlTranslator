f9199f5a0a77e8dfb733c5d7b16ae0d9942be460
ItsRainingHP <brenner682@live.com>
2024-11-23 14:49:27 -0800

    Update config loading

--- src/main/java/com/raindropcentral/YamlReader.java
+++ src/main/java/com/raindropcentral/YamlReader.java
@@ -6,7 +6,9 @@ import org.simpleyaml.configuration.file.YamlFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
@@ -41,6 +43,21 @@ public class YamlReader {
     public static Map<String, Object> loadConfig() throws URISyntaxException {
         Path jarPath = Paths.get(YamlReader.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParent();
         File yamlFile = jarPath.resolve("config.yml").toFile();
+
+        // Check if the config.yml file exists
+        if (!yamlFile.exists()) {
+            // If not, copy the default config.yml from resources
+            try (InputStream inputStream = YamlReader.class.getResourceAsStream("/config.yml")) {
+                if (inputStream == null) {
+                    throw new IOException("Default config.yml not found in resources");
+                }
+                Files.copy(inputStream, yamlFile.toPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
         try {
             YamlFile config = new YamlFile(yamlFile);
             config.loadWithComments();
